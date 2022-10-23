d76fbc324dc3f804f97425e832e3ecd618a2335c
rssabbir <sabbirhossainrahad@gmail.com>
2022-10-24 04:31:27 +0600

    under build

--- index.js
+++ index.js
@@ -3,16 +3,39 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
+const places = require('./data/places.json');
+const hotels = require('./data/hotels.json');
 
-const places = require('./data/places.json')
-
+//
+//
+//
+//
+//Places API
 app.get('/', (req, res) => {
 	res.send('travel guru backend running');
 });
 app.get('/places', (req, res) => {
-    res.send(places)
-})
+	res.send(places);
+});
+app.get('/places/:id', (req, res) => {
+	const id = req.params.id;
+	const place = places.find((place) => place.id == id);
+	res.json(place);
+});
 
+//
+//
+//
+//
+//Hotel API
+app.get('/hotels', (req, res) => {
+    res.send(hotels);
+})
+//
+//
+//
+//
+//Listen Server
 app.listen(port, () => {
-    console.log(`server is running on ${port}`);
+	console.log(`server is running on ${port}`);
 });
